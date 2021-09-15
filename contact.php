5797b22473d12aaec18890f6e7bbd953fdf16aa1
Md Abdul Karim <karim.cse007@gmail.com>
2021-09-15 13:20:42 +0600

    first commit

---
+++ contact.php
@@ -0,0 +1,102 @@
+         <!--------Contact section end--------> 
+                <div id="contact" class="page stellar">
+                    <div class="pageheader">
+                        <div class="headercontent">
+                            <div class="section-container">
+                                
+                                <h2 class="title">Contact & Meet Me</h2>
+                            
+                                <div class="row">
+                                    <div class="col-md-8">
+                                        <p>I would be happy to talk to you if you need my assistance in your research or whether you need bussiness administration support for your company. Though I have limited time for students but I .....</p>                              
+                                    </div>
+                                    <div class="col-md-4">
+                                        <ul class="list-unstyled">
+                                            <li>
+                                                <strong><i class="icon-phone"></i>&nbsp;&nbsp;</strong>
+                                                <span>office: +8801553200356</span>
+                                            </li>
+                                            <li>
+                                                <strong><i class="icon-phone"></i>&nbsp;&nbsp;</strong>
+                                                <span>lab: +8801553200356</span>
+                                            </li>
+                                            <li>
+                                                <strong><i class="icon-envelope"></i>&nbsp;&nbsp;</strong>
+                                                <span><a href="mailto:hanif_tushar.cse@uap-bd.edu" class="__cf_email__" data-cfemail="">hanif_tushar.cse@uap-bd.edu </a></span>
+                                            </li>
+                                            <li>
+                                                <strong><i class="icon-skype"></i>&nbsp;&nbsp;</strong>
+                                                <span>hanifbhuiyan.c</span>
+                                            </li>
+                                            <li>
+                                                <strong><i class="icon-facebook"></i>&nbsp;&nbsp;</strong>
+                                                <a href="http://facebook.com/villen09" target="_blank"><span>Hanif Bhuiyan</span></a>
+                                            </li>
+                                            <li>
+                                                <strong><i class="icon-twitter"></i>&nbsp;&nbsp;</strong><a href="https://twitter.com/Unknown_Tushar" target="_blank"><span>@Unknown_Tushar</span></a>
+                                                
+                                            </li>
+                                            <li>
+                                                <strong><i class="icon-linkedin-sign"></i>&nbsp;&nbsp;</strong>
+                                                <span>
+                                                    <a href="www.linkedin.com/in/hanif-bhuiyan-Tushar" target="_blank">hanif-bhuiyan-Tushar</a>
+                                                </span>
+                                            </li>
+                                        </ul>    
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pagecontents">
+                        <div class="section contact-office" data-stellar-background-ratio="0.1">
+                            <div class="section-container">
+                                <div class="row">
+                                    <div class="col-md-8">
+                                        <h2 class="title">At My Office</h2>
+                                        <p>You can find me at my office located at University of Asia Pacific, Dhaka, Bangladesh</p>
+                                        <p>I am at my office every day from 10:00 am until 5:00 pm, but you may consider a call to fix an appointment.</p>
+                                    </div>
+                                    <div class="col-md-4 text-center hidden-xs hidden-sm">
+                                        <i class="icon-coffee icon-huge"></i>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="section color-1">
+                            <div class="section-container">
+                                <div class="row">
+                                    <div class="col-md-4 text-center hidden-xs hidden-sm">
+                                        <i class="icon-stethoscope icon-huge"></i>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <h2 class="title">At My Work</h2>
+                                        <p>You can find me at my Work located at University of Asia Pacific, Dhaka, Bangladesh</p>
+                                        <p>I am at my office every day from 7:00 am until 5:00 pm, but you may consider a call to fix an appointment.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="section contact-lab" data-stellar-background-ratio="0.1">
+                            <div class="section-container">
+                                <div class="row">
+                                    
+                                    <div class="col-md-8">
+                                        <h2 class="title">At My Lab</h2>
+                                        <p>You can find me at my office located at University of Asia Pacific, Dhaka, Bangladesh</p>
+                                        <p>I am at my office every day from 10:00 amuntil 5:00 pm, but you may consider a call to fix an appointment.</p>
+                                    </div>
+                                    <div class="col-md-4 text-center hidden-xs hidden-sm">
+                                        <i class="icon-superscript icon-huge"></i>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+        <div id="overlay"></div>
+        <!--------Contact section end--------> 
